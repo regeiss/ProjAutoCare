a2a4b8c8698279fac38494ea24b91da17718bf6f
Roberto Edgar Geiss <15005694+regeiss@users.noreply.github.com>
2023-06-14 16:35:52 -0300

    itens da sidebar

--- ProjAutoCare/SupportingFiles/ContentView.swift
+++ ProjAutoCare/SupportingFiles/ContentView.swift
@@ -98,6 +98,83 @@ struct ContentView: View
     //    }
 }
 
+struct ItemsSideBar
+{
+        var body: some View
+    {
+        VStack(alignment: .leading)
+        {
+            HStack
+            {
+                Image(systemName: "rectangle.and.pencil.and.ellipsis")
+                    .foregroundColor(.gray)
+                    .imageScale(.large)
+                Text("Login")
+                    .foregroundColor(.gray)
+                    .font(.headline)
+            }
+            .padding(.top, 100)
+            .onTapGesture
+            {
+                showMenu = false
+                // router.toLogin()
+            }
+            HStack
+            {
+                Image(systemName: "person")
+                    .foregroundColor(.gray)
+                    .imageScale(.large)
+                Text("Perfil")
+                    .foregroundColor(.gray)
+                    .font(.headline)
+            }
+            .padding(.top, 30)
+            .onTapGesture
+            {
+                showMenu = false
+                // router.toListaPerfil()
+            }
+            HStack
+            {
+                Image(systemName: "envelope")
+                    .foregroundColor(.gray)
+                    .imageScale(.large)
+                Text("Mensagens")
+                    .foregroundColor(.gray)
+                    .font(.headline)
+            }
+            .padding(.top, 30)
+            .onTapGesture
+            {
+                showMenu = false
+                // router.toMenu()
+            }
+            HStack
+            {
+                Image(systemName: "gearshape")
+                    .foregroundColor(.gray)
+                    .imageScale(.large)
+                Text("Configurações")
+                    .foregroundColor(.gray)
+                    .font(.headline)
+            }
+            .padding(.top, 30)
+            .onTapGesture
+            {
+                showMenu = false
+                //pilot.push(.settings)
+            }
+            Spacer()
+            HStack
+            {
+                let appBuild = Bundle.main.infoDictionary?["CFBundleShortVersionString"] as? String
+                Text("WerkstadtG v.")
+                Text(appBuild ?? "1.0")
+                    .foregroundColor(.gray)
+            }.padding([.leading, .bottom])
+        }.background(Color(red: 32/255, green: 32/255, blue: 32/255))
+}
+
 #Preview
 {
     ContentView()
