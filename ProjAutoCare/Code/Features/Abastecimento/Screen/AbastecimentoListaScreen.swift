885eca9e57cea9d7897bff5551f2a8d9a7ae8aef
Roberto Edgar Geiss <15005694+regeiss@users.noreply.github.com>
2023-06-20 16:38:53 -0300

    @query

--- ProjAutoCare/Code/Features/Abastecimento/Screen/AbastecimentoListaScreen.swift
+++ ProjAutoCare/Code/Features/Abastecimento/Screen/AbastecimentoListaScreen.swift
@@ -6,36 +6,43 @@
 //
 
 import SwiftUI
+import SwiftData
 
 @available(iOS 16.0, *)
 struct AbastecimentoListaScreen: View
 {
+    @Query private var abastecimento: [Abastecimento]
+    @State private var adicao = false
+
     var body: some View
     {
         Text("Abastecimento")
-//        List
-//        {
-//            ForEach(abastecimentos) { abastecimento in
-//                HStack
-//                {
-//                    AbastecimentoListaDetalheView(abastecimento: abastecimento)
-//                }
-//            }
-//            .onDelete(perform: $abastecimentos.remove(atOffsets:))
-//            if abastecimentos.isEmpty
-//            {
-//                Text("").listRowBackground(Color.clear)
-//            }
-//        }
-//        .background(Color("backGroundMain"))
-//        .scrollContentBackground(.hidden)
-//        .navigationBarTitle("Abastecimento", displayMode: .automatic)
-//        .toolbar { ToolbarItem(placement: .navigationBarTrailing)
-//            { Button {
-//                pilot.push(.edicaoAbastecimento(abastecimento: Abastecimento()))
-//            }
-//                label: { Image(systemName: "plus")}}
-//        }
+       List
+       {
+           ForEach(abastecimentos) { abastecimento in
+               HStack
+               {
+                   AbastecimentoListaDetalheView(abastecimento: abastecimento)
+               }
+           }
+           .onDelete(perform: $abastecimentos.remove(atOffsets:))
+           if abastecimentos.isEmpty
+           {
+               Text("").listRowBackground(Color.clear)
+           }
+       }
+       .background(Color("backGroundMain"))
+       .scrollContentBackground(.hidden)
+       .navigationBarTitle("Abastecimento", displayMode: .automatic)
+       .toolbar { ToolbarItem(placement: .navigationBarTrailing)
+           { Button {
+               adicao = true 
+           }
+               label: { Image(systemName: "plus")}}
+       }
+       .navigationDestination(isPresented: $adicao, destination: {
+            AbastecimetnoScreen()
+        })
     }
 }
 
