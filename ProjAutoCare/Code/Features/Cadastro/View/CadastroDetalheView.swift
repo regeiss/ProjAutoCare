284deb1796c26e8f8c96bd176b88c5cfe0e7007a
Roberto <roberto.geiss.nh@icloud.com>
2023-06-29 12:08:26 -0300

    menus

---
+++ ProjAutoCare/Code/Features/Cadastro/View/CadastroDetalheView.swift
@@ -0,0 +1,18 @@
+//
+//  CadastroDetalheView.swift
+//  ProjAutoCare
+//
+//  Created by Roberto Edgar Geiss on 29/06/23.
+//
+
+import SwiftUI
+
+struct CadastroDetalheView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    CadastroDetalheView()
+}
---
+++ ProjAutoCare/Code/Features/Perfil/View/PerfilView.swift
@@ -0,0 +1,8 @@
+//
+//  PerfilView.swift
+//  ProjAutoCare
+//
+//  Created by Roberto Edgar Geiss on 29/06/23.
+//
+
+import Foundation
